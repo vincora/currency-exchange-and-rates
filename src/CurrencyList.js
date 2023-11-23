ec353d505264f8ed5ae6d6ffae4a29d9fe401ca6
Yulia Glagola <julichis@mail.ru>
2023-11-23 19:50:16 +0400

    env variable added

--- src/CurrencyList.js
+++ src/CurrencyList.js
@@ -1,36 +1,37 @@
 import React, { useState, useEffect } from "react";
-import { apiKey } from "./apiKey";
 
 const fetchData = (baseCurrency = 'USD') => {
-  const apiUrl = `https://openexchangerates.org/api/latest.json?app_id=${apiKey}&base=${baseCurrency}`;
-
-  return fetch(apiUrl)
+    const apiKey = process.env.REACT_APP_OPEN_EXCHANGE_RATES_API_KEY;
+    const apiUrl = `https://openexchangerates.org/api/latest.json?app_id=${apiKey}&base=${baseCurrency}`;
+    
+    return fetch(apiUrl)
     .then((response) => {
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-      return response.json();
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
     })
     .then((data) => data.rates)
     .catch((err) => console.error(err.message));
 };
 
 const CurrencyList = () => {
-  const [currList, setCurrList] = useState({});
-  useEffect(() => {
-    fetchData()
-      .then((data) => setCurrList(data))
-      .catch((err) => console.log(`Error in useEffect: ${err.message}`));
-  }, []);
-
-  return (
-    <ul>
-      {currList &&
-        Object.keys(currList).map((key) => (
-          <li key={key}>{`${key}: ${currList[key]}`}</li>
-        ))}
-    </ul>
-  );
-};
-
-export default CurrencyList;
+    const [currList, setCurrList] = useState({});
+    useEffect(() => {
+        fetchData()
+        .then((data) => setCurrList(data))
+        .catch((err) => console.log(`Error in useEffect: ${err.message}`));
+    }, []);
+    
+    return (
+        <ul>
+        {currList &&
+            Object.keys(currList).map((key) => (
+                <li key={key}>{`${key}: ${currList[key]}`}</li>
+                ))}
+                </ul>
+                );
+            };
+            
+            export default CurrencyList;
+            
