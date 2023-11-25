f4230f0ead7f1dd79321735f4422fa64d519e814
Yulia Glagola <julichis@mail.ru>
2023-11-25 18:50:02 +0400

    added react-router-dom 6

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import {Routes, Route, Link} from 'react-router-dom';
 import CurrencyList from "./CurrencyList";
+import Converter from './Converter';
 
 function App() {
   return (
-    <div className="App">
-      <CurrencyList/>
-    </div>
+    <>
+    <nav>
+      <ul>
+        <li><Link to='/'>List</Link></li>
+        <li><Link to='/converter'>Converter</Link></li>
+      </ul>
+    </nav>
+    <Routes>
+      <Route path="/" element={<CurrencyList />}/>
+      <Route path="/converter" element={<Converter/>}/>
+    </Routes>
+    </>
+    
   );
 }
 
--- src/CurrencyList.js
+++ src/CurrencyList.js
@@ -1,37 +1,36 @@
 import React, { useState, useEffect } from "react";
 
-const fetchData = (baseCurrency = 'USD') => {
-    const apiKey = process.env.REACT_APP_OPEN_EXCHANGE_RATES_API_KEY;
-    const apiUrl = `https://openexchangerates.org/api/latest.json?app_id=${apiKey}&base=${baseCurrency}`;
-    
-    return fetch(apiUrl)
+const fetchData = (baseCurrency = "USD") => {
+  const apiKey = process.env.REACT_APP_OPEN_EXCHANGE_RATES_API_KEY;
+  const apiUrl = `https://openexchangerates.org/api/latest.json?app_id=${apiKey}&base=${baseCurrency}`;
+
+  return fetch(apiUrl)
     .then((response) => {
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        return response.json();
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json();
     })
     .then((data) => data.rates)
     .catch((err) => console.error(err.message));
 };
 
 const CurrencyList = () => {
-    const [currList, setCurrList] = useState({});
-    useEffect(() => {
-        fetchData()
-        .then((data) => setCurrList(data))
-        .catch((err) => console.log(`Error in useEffect: ${err.message}`));
-    }, []);
-    
-    return (
-        <ul>
-        {currList &&
-            Object.keys(currList).map((key) => (
-                <li key={key}>{`${key}: ${currList[key]}`}</li>
-                ))}
-                </ul>
-                );
-            };
-            
-            export default CurrencyList;
-            
+  const [currList, setCurrList] = useState({});
+  useEffect(() => {
+    fetchData()
+      .then((data) => setCurrList(data))
+      .catch((err) => console.log(`Error in useEffect: ${err.message}`));
+  }, []);
+
+  return (
+    <ul>
+      {currList &&
+        Object.keys(currList).map((key) => (
+          <li key={key}>{`${key}: ${currList[key]}`}</li>
+        ))}
+    </ul>
+  );
+};
+
+export default CurrencyList;
