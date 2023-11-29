b9c8bf03862967fa6bb700d75e902316ea8859a0
Yulia Glagola <julichis@mail.ru>
2023-11-29 17:52:39 +0400

    refactoring

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,14 @@ import { Outlet, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className='container w-3/4 p-4'>
+    <div className="container w-3/4 p-4">
       <nav className="mb-6">
-        <ul className='flex justify-center gap-4 text-sky-700 text-lg font-medium'>
-          <li>
-            <Link to="/converter">Converter</Link>
-          </li>
+        <ul className="flex justify-center gap-4 text-sky-700 text-lg font-medium">
           <li>
             <Link to="/list">Exchange rates</Link>
+          </li>
+          <li>
+            <Link to="/converter">Converter</Link>
           </li>
         </ul>
       </nav>
