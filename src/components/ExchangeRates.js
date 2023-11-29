097f86330f7ec62fe512fd5ce5ec5891818c24ba
Yulia Glagola <julichis@mail.ru>
2023-11-29 22:37:33 +0400

    rewrote api request using react query

--- src/components/ExchangeRates.js
+++ src/components/ExchangeRates.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import {
   Select,
   SelectContent,
@@ -14,60 +14,45 @@ import {
   TableHeader,
   TableRow,
 } from "./ui/table";
+import { useQuery } from "@tanstack/react-query";
 
 const apiKey = process.env.REACT_APP_OPEN_EXCHANGE_RATES_API_KEY;
 
-const fetchRates = async (baseCurrency = "USD") => {
-  const apiUrl = `https://openexchangerates.org/api/latest.json?app_id=${apiKey}&base=${baseCurrency}`;
-
-  try {
-    const response = await fetch(apiUrl);
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    const data = await response.json();
-    return data.rates;
-  } catch (err) {
-    return console.error(err.message);
-  }
+const fetchRates = async () => {
+  const apiUrl = `https://openexchangerates.org/api/latest.json?app_id=${apiKey}`;
+  const response = await fetch(apiUrl);
+  const data = await response.json();
+  return data.rates;
 };
+
 const fetchCurrencies = async () => {
   const options = { method: "GET", headers: { accept: "application/json" } };
-
-  try {
-    const response = await fetch(
-      `https://openexchangerates.org/api/currencies.json?prettyprint=false&show_alternative=false&show_inactive=false&app_id=${apiKey}`,
-      options
-    );
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    const data = await response.json();
-    return data;
-  } catch (err) {
-    return console.error(err.message);
-  }
+  const response = await fetch(
+    `https://openexchangerates.org/api/currencies.json?prettyprint=false&show_alternative=false&show_inactive=false&app_id=${apiKey}`,
+    options
+  );
+  const data = await response.json();
+  return data;
 };
 
-const ExchangeRates = () => {
-  const [ratesList, setRatesList] = useState({});
-  const [baseCurrency, setBaseCurrency] = useState("USD");
-  const [curr, setCurr] = useState({});
 
-  useEffect(() => {
-    fetchRates()
-      .then((data) => setRatesList(data))
-      .catch((err) =>
-        console.log(`Error in useEffect fetching rates: ${err.message}`)
-      );
-    fetchCurrencies()
-      .then((data) => setCurr(data))
-      .catch((err) =>
-        console.log(
-          `Error in useEffect fetching currencies list: ${err.message}`
-        )
-      );
-  }, []);
+const ExchangeRates = () => {
+  const [baseCurrency, setBaseCurrency] = useState("USD");
+  const ratesQuery = useQuery({ queryKey: ["ratesList"], queryFn: fetchRates });
+  const currenciesQuery = useQuery({
+    queryKey: ["currencies"],
+    queryFn: fetchCurrencies,
+  });
+
+  if (ratesQuery.isLoading) {
+    return <h3>Loading...</h3>;
+  }
+  if (ratesQuery.isError) {
+    return <h3>{JSON.stringify(ratesQuery.error)}</h3>;
+  }
+  if (!ratesQuery.data){
+    return <h3>No data</h3>
+  }
 
   return (
     <div>
@@ -81,10 +66,10 @@ const ExchangeRates = () => {
             <SelectValue />
           </SelectTrigger>
           <SelectContent>
-            {curr &&
-              Object.keys(curr).map((key) => (
+            {currenciesQuery.data &&
+              Object.keys(currenciesQuery.data).map((key) => (
                 <SelectItem key={key} value={key}>
-                  {curr[key]}
+                  {currenciesQuery.data[key]}
                 </SelectItem>
               ))}
           </SelectContent>
@@ -99,13 +84,15 @@ const ExchangeRates = () => {
           </TableRow>
         </TableHeader>
         <TableBody>
-          {ratesList &&
-            Object.keys(ratesList).map((key) => (
+          {
+            Object.keys(ratesQuery.data).map((key) => (
               <TableRow key={key}>
                 <TableCell>{key}</TableCell>
-                <TableCell>{curr[key]}</TableCell>
+                <TableCell>{currenciesQuery.data && currenciesQuery.data[key]}</TableCell>
                 <TableCell className="text-right">
-                  {(ratesList[key] / ratesList[baseCurrency]).toFixed(2)}
+                  {(
+                    ratesQuery.data[key] / ratesQuery.data[baseCurrency]
+                  ).toFixed(2)}
                 </TableCell>
               </TableRow>
             ))}
