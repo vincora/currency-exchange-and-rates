9493457aa77f762135220d2978d1e60d9133cd15
Yulia Glagola <julichis@mail.ru>
2023-11-29 23:16:31 +0400

    separated fetching functions in separated files

---
+++ src/components/fetchRates.js
@@ -0,0 +1,10 @@
+const apiKey = process.env.REACT_APP_OPEN_EXCHANGE_RATES_API_KEY;
+
+const fetchRates = async () => {
+  const apiUrl = `https://openexchangerates.org/api/latest.json?app_id=${apiKey}`;
+  const response = await fetch(apiUrl);
+  const data = await response.json();
+  return data.rates;
+};
+
+export default fetchRates;
